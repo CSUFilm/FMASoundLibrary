4df13b28a27c7c3f2e2ef9f71c0ec6527f4f840f
Zachary A Kascak <zak@dyn-host-137-148-169-204.csuohio.edu>
2018-06-20 16:49:35 -0400

    Added genre 3 search

--- soundsearch.php
+++ soundsearch.php
@@ -96,5 +96,37 @@ if ($genre2NumRows < 1) {
 	}
 }
 
+$genre3Results = mysqli_query($mysqli, "SELECT * FROM sfx WHERE genre3 LIKE '%$searchText%';");
+$genre3NumRows = mysqli_num_rows($genre3Results);
+
+echo "--------------------GENRE 3 RESULTS--------------------\n";
+
+if ($genre3NumRows < 1) {
+	echo "No results found.\n\n";
+} else {
+	echo "Number of results: " . $genre3NumRows . "\n\n";
+
+	while ($records = mysqli_fetch_array($genre3Results)){
+		$sfx_id = $records['sfx_id'];
+		$cd_name = $records['cd_name'];
+		$cd_track = $records['cd_track'];
+		$cd_track_index = $records['cd_track_index'];
+		$title = $records['title'];
+		$genre1 = $records['genre1'];
+		$genre2 = $records['genre2'];
+		$genre3 = $records['genre3'];
+		$description = $records['description'];
+		$length = $records['length'];
+		$location = $records['location'];
+		$license_id = $records['license_id'];
+		
+		echo "SFX ID: $sfx_id \t CD: $cd_name \t CD Track: $cd_track \t CD Track Index: $cd_track_index \n";
+		echo "Title: $title \n";
+		echo "Genres: $genre1 \t $genre2 \t $genre3 \n";
+		echo "Description: $description \n";
+		echo "Length: $length \t Location: /$location \n\n";
+	}
+}
+
 mysqli_close($mysqli);
 ?>
